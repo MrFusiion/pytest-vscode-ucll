776a12091bbe8d83de6b5c2dfb0ce38e5c35aec0
jesse-ucll <jesse.vandenbempt@student.ucll.be>
2023-03-04 22:04:08 +0100

    fix handle assigments.md with wrong names

--- src/AssignmentManager.ts
+++ src/AssignmentManager.ts
@@ -1,8 +1,14 @@
-import { Uri, ExtensionContext } from "vscode";
+import { ExtensionContext, RelativePattern, workspace } from "vscode";
 import * as path from "path";
 import { Disposable } from "./util/Disposable";
 import { MarkdownPreviewManager } from "./markdown/MarkdownPreviewManager";
 import { IMarkdownPreviewPanelOptions, MarkdownPreviewPanel } from "./markdown/MarkdownPreviewPanel";
+
+const ASSIGMENT_NAMES = [
+    "assignment.md",
+    "explanation.md",
+    "*.md" // fallback for when the assignment is not named correctly
+]
 
 export class AssignmentManager extends Disposable {
 
@@ -13,13 +19,17 @@ export class AssignmentManager extends Disposable {
         this._markdownPreviewManager = this._register(new MarkdownPreviewManager(context));
     }
 
-    public async open(folder: string, options?: IMarkdownPreviewPanelOptions): Promise<MarkdownPreviewPanel> {
-        return this._markdownPreviewManager.getMarkdownPanel(
-            Uri.file(path.join(folder, "assignment.md")),
-            {
-                name: `Assignment: ${path.basename(folder)}`,
-                ...options
+    public async open(folder: string, options?: IMarkdownPreviewPanelOptions): Promise<MarkdownPreviewPanel|null> {
+        for (const name of ASSIGMENT_NAMES) {
+            const pattern = new RelativePattern(folder, name);
+            const files = await workspace.findFiles(pattern);
+            if (files.length > 0) {
+                return this._register(this._markdownPreviewManager.getMarkdownPanel(files[0], {
+                    name: `Assignment: ${path.basename(folder)}`,
+                    ...options
+                }));
             }
-        );
+        }
+        return null;
     }
 }
